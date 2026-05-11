09607271d436256255ee78327254aa070f242fbb
evily kauane <20231ireinfint0019@ifba.edu.br>
2026-05-11 10:10:11 -0300

    adicionamos o tratamento de exceções com o while para aparecer as informações da tabela

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,37 @@
 import db.DB;
 
+import java.sql.Statement;
 import java.sql.Connection;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Connection conn = DB.getConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
 
-        System.out.println("conexão realizada com muito sucesso");
-        System.out.println(DB.getConnection());
-        DB.closeConnection();
+        try {
+         conn= DB.getConnection();
 
+         st = conn.createStatement();
+
+         rs = st.executeQuery("select * from department");
+
+         while (rs.next()){// enquanto estiver verdadeiro ele continua, quando chegar na última posição ele verifica se tem mais
+             //alguma coisa, ai rertona falso;
+             System.out.println(rs.getInt("Id") + "-"+ rs.getString("Name"));
+
+         }
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
+
+    }
     }
